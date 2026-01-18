9ced86dc8e23012e36d543f3f02ffef0233f2842
HaiMInhLe <haiminh005@gmail.com>
2026-01-18 10:17:38 +0700

    Fixed the variables' name, tested the copy_worldfunction for the copy part only. I haven't test the error handling part, and I rework the description for the script

--- src/mcserver-backup.py
+++ src/mcserver-backup.py
@@ -9,7 +9,7 @@ from uuid import uuid4
 # main logic of the code 
 def main():
     # Parse the argument from the command line
-    parser = argparse.ArgumentParser(prog = 'backup_script', description='A script that compress using xz -9e and backup the minecraft server directory, run periodically using cron')
+    parser = argparse.ArgumentParser(prog = 'backup_script', description='* A script that compress using xz -9e and backup the minecraft server directory, run periodically using cron or systemd timer\n* Example usage: python3 mcserver-backup.py ~/server_dir /mnt/backup_dir')
     parser.add_argument('source', help='The source directory')
     parser.add_argument('destination', help='the destination directory that you want to save the compressed backup file')
 
@@ -22,7 +22,8 @@ def main():
     # Add in time and date 
     current_time = datetime.now().strftime("%Y-%m-%d_%H%M%S")
     backup_filename = dest / f"mc_backup_{current_time}.tar.xz"
-    # print(backup_filename)
+    
+    copy_world(source)
 
 def pause_server():
     # Tell minecraft to stop writing to disk
@@ -32,23 +33,24 @@ def pause_server():
     run(["tmux", "send-keys", "-t", "MC", "save-all", "Enter"])
     time.sleep(120)
 
-def copy_world(src):
+# TESTED for the copy part, hasn't tested for the error handling part
+def copy_world(copy_src):
     last_error = None
     
     for attempt in range(3):
 
         # Create a tmp dierctory using an unique ID each time the code retry or run
         snapshot_id = uuid4().hex
-        dst = Path(f"/tmp/mcServerCopyTemp{snapshot_id}/") 
+        copy_dst = Path(f"/tmp/mcServerCopyTemp_{snapshot_id}/") 
         try:
-            copytree(src, dst)
-            return dst
+            copytree(copy_src, copy_dst)
+            return copy_dst
 
         # Raise an exception if copytree fail, and follow up with clean up and retry
         except Exception as e:
             # Check if the directory dst exist or not. If yes, then delete it here
-            if dst.exists():
-                rmtree(dst)
+            if copy_dst.exists():
+                rmtree(copy_dst)
             last_error = e
             # Will use a dedicated log module later on
             print("Attempt fail, retry")
@@ -59,12 +61,12 @@ def resume_server():
     run(["tmux", "send-keys", "-t", "MC", "save-on", "Enter"])
 
 # It is a live backup so I think I will remove the compression program thing, as it will use all the available threads and leave none for the minecraft server. One or two threads for this should be sufficient
-# Need to work on renaming variables dst so it means different thing in differnt functions
-def compress_backup(src, dst):
-    run(["tar", "-cJf", dst, "--exclude=./logs", "--exclude=./cache", "--exclude=./libraries", f"--directory={src}"])
+# Need to work on renaming variables dst so it means different thing in different functions
+def compress_backup(compress_src, compress_dst):
+    run(["tar", "-cJf", compress_dst, "--exclude=./logs", "--exclude=./cache", "--exclude=./libraries", f"--directory={compress_src}"])
 
-def cleanup_temp(dst):
-    rmtree(dst) 
+def cleanup_temp(temp_dst):
+    rmtree(temp_dst) 
 
 
 
